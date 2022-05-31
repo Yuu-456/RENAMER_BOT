59ca43e9bd4afebe4b8c17f3e15c4f74b8385d35
Izumi Tachibana <99785450+Yuu-456@users.noreply.github.com>
2022-05-31 13:36:09 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -29,8 +29,8 @@ async def start(client,message):
 	**Document Or Video** and enter new filenameto rename it__
 	""",reply_to_message_id = message.message_id ,  
 	reply_markup=InlineKeyboardMarkup(
-	 [[ InlineKeyboardButton("Support 🇮🇳" ,url="https://t.me/lntechnical") ], 
-	[InlineKeyboardButton("Subscribe 🧐", url="https://youtube.com/c/LNtechnical") ]  ]))
+	 [[ InlineKeyboardButton("Anime Uploadz 🇮🇳" ,url="https://t.me/Anime_Uploadz") ], 
+	[InlineKeyboardButton("Void Network 🧐", url="https://t.me/VoidXNetwork") ]  ]))
 
 
 
